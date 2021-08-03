22f06eed23fffa494141507576042ca47ca9cbfd
Alireza Toghyiani <mr.toghyiani@icloud.com>
2021-08-03 10:37:47 +0430

    Initial Commit

---
+++ WSR-Asia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WSR-Asia
+//
+//  Created by ali on 8/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
