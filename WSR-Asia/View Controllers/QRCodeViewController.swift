4c048247c110b6be586082540f385113997f5ac1
Alireza Toghyiani <mr.toghyiani@icloud.com>
2021-08-04 10:57:08 +0430

    makke qr code view

---
+++ WSR-Asia/View Controllers/QRCodeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeViewController.swift
+//  WSR-Asia
+//
+//  Created by ali on 8/4/21.
+//
+
+import UIKit
+
+class QRCodeViewController: UIViewController, Storyboarded {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
